e3db13e0b9ea37561b6f2530b75e3bf1a7bcbadc
Davydovich, Roman <r.davydovich@itransition.com>
2017-05-17 14:12:06 +0300

    Added PreProcessor project

---
+++ AutoTests.Framework/AutoTests.Framework.PreProcessor/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoTests.Framework.PreProcessor")]
+[assembly: AssemblyProduct("AutoTests.Framework")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1fb0d9f1-c585-48ff-a22b-74719ccead38")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
