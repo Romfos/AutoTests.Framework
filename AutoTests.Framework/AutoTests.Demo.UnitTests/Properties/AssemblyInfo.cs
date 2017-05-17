2f96a0e7156a65f5a1c26669cf72d4b2ba78a5cb
Davydovich, Roman <r.davydovich@itransition.com>
2017-05-17 11:57:12 +0300

    Initial commit

---
+++ AutoTests.Framework/AutoTests.Demo.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoTests.Demo.UnitTests")]
+[assembly: AssemblyProduct("AutoTests.Demo")]
+[assembly: ComVisible(false)]
+[assembly: Guid("375e256d-7729-42c1-a7d7-796f92bc0ba0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
---
+++ AutoTests.Framework/AutoTests.Framework.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoTests.Framework.Core")]
+[assembly: AssemblyProduct("AutoTests.Framework")]
+[assembly: ComVisible(false)]
+[assembly: Guid("04b3ea91-2351-4b90-b41f-e26a5b35285c")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
