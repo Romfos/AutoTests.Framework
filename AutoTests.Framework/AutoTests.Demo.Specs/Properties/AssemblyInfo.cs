5428417950ec5524c0ebd6e30954c8eb78a8b9fc
Davydovich, Roman <r.davydovich@itransition.com>
2017-05-18 13:08:22 +0300

    Update version to 0.5

--- AutoTests.Framework/AutoTests.Demo.Specs/Properties/AssemblyInfo.cs
+++ AutoTests.Framework/AutoTests.Demo.Specs/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AutoTests.Demo")]
 [assembly: ComVisible(false)]
 [assembly: Guid("016aafe8-8e2f-4ca0-9142-d4e4071d49e3")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
--- AutoTests.Framework/AutoTests.Demo.UnitTests/Properties/AssemblyInfo.cs
+++ AutoTests.Framework/AutoTests.Demo.UnitTests/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AutoTests.Demo")]
 [assembly: ComVisible(false)]
 [assembly: Guid("375e256d-7729-42c1-a7d7-796f92bc0ba0")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
