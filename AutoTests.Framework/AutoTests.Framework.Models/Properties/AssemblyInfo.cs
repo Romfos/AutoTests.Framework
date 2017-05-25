91465e911ea90563b7558d903b4939acd1f2a5c2
Davydovich, Roman <r.davydovich@itransition.com>
2017-05-25 12:29:45 +0300

    Release 0.6.4

--- AutoTests.Framework/AutoTests.Framework.Models/Properties/AssemblyInfo.cs
+++ AutoTests.Framework/AutoTests.Framework.Models/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AutoTests.Framework")]
 [assembly: ComVisible(false)]
 [assembly: Guid("05ba50f8-ec90-406d-af94-aee1bebf0998")]
-[assembly: AssemblyVersion("0.6.3.0")]
-[assembly: AssemblyFileVersion("0.6.3.0")]
+[assembly: AssemblyVersion("0.6.4.0")]
+[assembly: AssemblyFileVersion("0.6.4.0")]
