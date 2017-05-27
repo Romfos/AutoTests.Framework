5abc87aadb4ba3efdc92cae1d066eae868834115
Roman Davydovich <Romfos@tut.by>
2017-05-27 23:37:04 +0300

    Release 0.9.0

--- AutoTests.Framework/AutoTests.Framework.PreProcessor/Properties/AssemblyInfo.cs
+++ AutoTests.Framework/AutoTests.Framework.PreProcessor/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AutoTests.Framework")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1fb0d9f1-c585-48ff-a22b-74719ccead38")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
