471e3142f9d87047ad9c15f98d67f63bbc430bed
Roman Davydovich <Romfos@tut.by>
2017-05-28 20:38:33 +0300

    Update build scripts

--- AutoTests.Framework/.nuget/publish.bat
+++ AutoTests.Framework/.nuget/publish.bat
@@ -1,4 +1,5 @@
 del *.nupkg
+call rebuild.bat
 call assembly.bat
 set /p KEY=<credentials.txt
 nuget push *.nupkg %KEY% -Source https://www.nuget.org/api/v2/package
---
+++ AutoTests.Framework/.nuget/rebuild.bat
@@ -0,0 +1,6 @@
+Path = C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
+MSBuild ..\AutoTests.Framework.Core\AutoTests.Framework.Core.csproj /p:Configuration=Release
+MSBuild ..\AutoTests.Framework.Models\AutoTests.Framework.Models.csproj /p:Configuration=Release
+MSBuild ..\AutoTests.Framework.PreProcessor\AutoTests.Framework.PreProcessor.csproj /p:Configuration=Release
+MSBuild ..\AutoTests.Framework.Web\AutoTests.Framework.Web.csproj /p:Configuration=Release
+MSBuild ..\AutoTests.Framework.TestData\AutoTests.Framework.TestData.csproj /p:Configuration=Release
