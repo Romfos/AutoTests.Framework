6ee71de77d70d60de98ec25a043a7111ef480c49
Davydovich, Roman <r.davydovich@itransition.com>
2017-06-02 11:03:04 +0300

    Added AutoTests.Tools.Refactroings and AutoTests.Tools.Tests projects

---
+++ AutoTests.Framework/AutoTests.Tools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoTests.Tools.Tests")]
+[assembly: AssemblyProduct("AutoTests.Tools.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b494f419-a288-45e6-9a37-8df0fb3130dc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
