ff7f7c8a2710a59962f0a9a7aca6ff1219b26ae1
Davydovich, Roman <r.davydovich@itransition.com>
2017-06-02 11:36:56 +0300

    Added StepDefinitionParser

---
+++ AutoTests.Framework/AutoTests.Tools.Tests/Steps/PlaygroundSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace AutoTests.Tools.Tests.Steps
+{
+    [Binding]
+    public class PlaygroundSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ AutoTests.Framework/AutoTests.Tools.Tests/Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Reflection;
+using AutoTests.Tools.Refactroings.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoTests.Tools.Tests.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void StepDefinitionParserTest()
+        {
+            var parser = new StepDefinitionParser();
+            var stepDefinitions = parser.Parse(Assembly.GetExecutingAssembly()).ToArray();
+
+            Assert.AreEqual(3, stepDefinitions.Length);
+        }
+    }
+}
