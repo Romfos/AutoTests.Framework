a77be33f4cd656be0c858747d3dd55c956b3cbbf
Roman Davydovich <Romfos@tut.by>
2017-07-07 21:21:04 +0300

    Added CommonContext

---
+++ AutoTests.Framework/AutoTests.Framework.Web/Common/CommonContext.cs
@@ -0,0 +1,89 @@
+﻿using OpenQA.Selenium;
+
+namespace AutoTests.Framework.Web.Common
+{
+    public class CommonContext : Context
+    {
+        protected IWebDriver Driver { get; }
+
+        public CommonContext(WebDependencies dependencies)
+        {
+            Driver = dependencies.WebDriverFactory.CreateWebDriver();
+        }
+
+        public void Navigate(string url)
+        {
+            Driver.Navigate().GoToUrl(url);
+        }
+
+        public void Refresh()
+        {
+            Driver.Navigate().Refresh();
+        }
+
+        public void Back()
+        {
+            Driver.Navigate().Back();
+        }
+
+        public void Forward()
+        {
+            Driver.Navigate().Forward();
+        }
+
+        public void Click(string locator)
+        {
+            Driver.FindElement(By.XPath(locator)).Click();
+        }
+
+        public void SetValue(string locator, string value)
+        {
+            Driver.FindElement(By.XPath(locator)).SendKeys(value);
+        }
+
+        public string GetAttribute(string locator, string attributeName)
+        {
+            return Driver.FindElement(By.XPath(locator)).GetAttribute(attributeName);
+        }
+
+        public void Submit(string locator)
+        {
+            Driver.FindElement(By.XPath(locator)).Submit();
+        }
+
+        public string GetTitle()
+        {
+            return Driver.Title;
+        }
+
+        public bool IsEnabled(string locator)
+        {
+            return Driver.FindElement(By.XPath(locator)).Enabled;
+        }
+
+        public bool IsSelected(string locator)
+        {
+            return Driver.FindElement(By.XPath(locator)).Selected;
+        }
+
+        public string GetText(string locator)
+        {
+            return Driver.FindElement(By.XPath(locator)).Text;
+        }
+
+        public bool IsDisplayed(string locator)
+        {
+            return Driver.FindElement(By.XPath(locator)).Displayed;
+        }
+
+        public void Clear(string locator)
+        {
+            Driver.FindElement(By.XPath(locator)).Clear();
+        }
+
+        public object Execute(string script, params object[] args)
+        {
+            return ((IJavaScriptExecutor) Driver).ExecuteScript(script, args);
+        }
+    }
+}
