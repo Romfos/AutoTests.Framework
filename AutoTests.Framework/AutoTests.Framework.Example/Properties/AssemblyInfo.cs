4000f8214ced7ba0ac5034bc5b4a61538b1262d5
Davydovich, Roman <r.davydovich@itransition.com>
2018-05-16 14:49:38 +0300

    Added AutoTests.Framework.Example project

---
+++ AutoTests.Framework/AutoTests.Framework.Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoTests.Framework.Example")]
+[assembly: AssemblyProduct("AutoTests.Framework")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5ec98f64-3c2d-403f-a322-8f3a211d9592")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
