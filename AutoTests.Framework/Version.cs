87208175d6988a3e9eb3d91cce639b17afd8189a
Davydovich, Roman <r.davydovich@itransition.com>
2018-10-19 15:34:04 +0300

    Release 2.4.1

--- AutoTests.Framework/Version.cs
+++ AutoTests.Framework/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
