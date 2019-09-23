7c364806621fa772fa0b4db20f56e0a848b762f2
Roman Davydovich <Roman.Davydovich@p97.com>
2019-09-23 13:08:49 +0300

    Added AutoTests.Framework.PreProcessor

---
+++ AutoTests.Framework/AutoTests.Framework.PreProcessor/IPreProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace AutoTests.Framework.PreProcessor
+{
+    public interface IPreProcessor
+    {
+        Task ExecuteAsync(string source);
+        Task<T> ExecuteAsync<T>(string source);
+    }
+}
