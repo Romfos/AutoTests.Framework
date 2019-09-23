ae5c822f671a685956c6ec96e3f0fa2968a6978e
Roman Davydovich <Roman.Davydovich@p97.com>
2019-09-23 15:03:57 +0300

    Added AutoTests.Framework.PreProcessor.Roslyn

---
+++ AutoTests.Framework/AutoTests.Framework.PreProcessor.Roslyn/RoslynPreProcessor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Threading.Tasks;
+
+namespace AutoTests.Framework.PreProcessor.Roslyn
+{
+    public class RoslynPreProcessor : IPreProcessor
+    {
+        private readonly object globals;
+        private readonly ScriptOptions scriptOptions;
+
+        public RoslynPreProcessor(object globals, ScriptOptions scriptOptions = null)
+        {
+            this.globals = globals;
+            this.scriptOptions = scriptOptions ?? ScriptOptions.Default;
+        }
+
+        public async Task ExecuteAsync(string code)
+        {
+            await CSharpScript.EvaluateAsync(code, scriptOptions, globals);
+        }
+
+        public async Task<T> ExecuteAsync<T>(string code)
+        {
+            return await CSharpScript.EvaluateAsync<T>(code, scriptOptions, globals);
+        }
+    }
+}
--- AutoTests.Framework/AutoTests.Framework.PreProcessor/IPreProcessor.cs
+++ AutoTests.Framework/AutoTests.Framework.PreProcessor/IPreProcessor.cs
@@ -4,7 +4,7 @@ namespace AutoTests.Framework.PreProcessor
 {
     public interface IPreProcessor
     {
-        Task ExecuteAsync(string source);
-        Task<T> ExecuteAsync<T>(string source);
+        Task ExecuteAsync(string code);
+        Task<T> ExecuteAsync<T>(string code);
     }
 }
