88838185e8f591cef84b2c2e93a9d723e212fe7e
Romfos <Romfos@tut.by>
2021-11-08 18:17:35 +0300

    Migrate to .NET 6

--- AutoTests.Framework/.build/pack.bat
+++ AutoTests.Framework/.build/pack.bat
@@ -1,13 +1,2 @@
 set /p Version=<Version.txt
-dotnet pack ..\AutoTests.Framework.Core\AutoTests.Framework.Core.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Core.Specflow\AutoTests.Framework.Core.Specflow.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.PreProcessor\AutoTests.Framework.PreProcessor.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.PreProcessor.Roslyn\AutoTests.Framework.PreProcessor.Roslyn.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.PreProcessor.Specflow\AutoTests.Framework.PreProcessor.Specflow.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Components\AutoTests.Framework.Components.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Components.Routes\AutoTests.Framework.Components.Routes.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Components.Specflow\AutoTests.Framework.Components.Specflow.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Data\AutoTests.Framework.Data.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Models\AutoTests.Framework.Models.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.Models.Specflow\AutoTests.Framework.Models.Specflow.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
-dotnet pack ..\AutoTests.Framework.All\AutoTests.Framework.All.csproj -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build" -p:TargetFrameworks=netstandard2.0
+dotnet pack ..\AutoTests.Framework.sln -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build"
