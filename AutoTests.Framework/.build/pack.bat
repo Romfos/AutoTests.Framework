ce9ea67387badd8bbe84370e7e65a579704e3323
Romfos <Romfos@tut.by>
2021-11-23 23:45:33 +0300

    Update Version mechanism

--- AutoTests.Framework/.build/pack.bat
+++ AutoTests.Framework/.build/pack.bat
@@ -1,2 +1,1 @@
-set /p Version=<Version.txt
-dotnet pack ..\AutoTests.Framework.sln -p:Configuration=Release -p:PackageVersion=%Version% -p:version=%Version% -o "../.build"
+dotnet pack ..\AutoTests.Framework.sln -p:Configuration=Release -o "../.build"
