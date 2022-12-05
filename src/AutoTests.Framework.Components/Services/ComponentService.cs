b163189e5203984c15fcbc6912f49910ad36a7fb
Romfos <Romfos@tut.by>
2022-12-05 14:04:39 +0100

    Update error messages

--- src/AutoTests.Framework.Components/Services/ComponentService.cs
+++ src/AutoTests.Framework.Components/Services/ComponentService.cs
@@ -25,7 +25,7 @@ public sealed class ComponentService
 
         if (component is not T result)
         {
-            throw new Exception($"Component by path {path} do not implement contract {typeof(T).FullName}");
+            throw new Exception($"Component by path '{path}' do not implement contract '{typeof(T).FullName}'");
         }
 
         return result;
@@ -42,17 +42,17 @@ public sealed class ComponentService
 
         if (properties.Count == 0)
         {
-            throw new Exception($"Unable to get route {routeName} from type {type.FullName}");
+            throw new Exception($"Unable to get route '{routeName}' from type '{type.FullName}'");
         }
 
         if (properties.Count > 1)
         {
-            throw new Exception($"Type {type.FullName} has several properties wthi the same route {routeName}");
+            throw new Exception($"Type '{type.FullName}' has several properties wthi the same route '{routeName}'");
         }
 
         var property = properties.Single();
         var routeComponent = property.GetValue(component);
 
-        return routeComponent ?? throw new Exception($"Property {property.Name} in {type.FullName} is null");
+        return routeComponent ?? throw new Exception($"Property '{property.Name}' in '{type.FullName}' is null");
     }
 }
