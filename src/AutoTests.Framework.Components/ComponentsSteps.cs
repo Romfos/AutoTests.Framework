555e4f0c869fb178b6d04365edf99cf086911daa
Romfos <Romfos@tut.by>
2022-12-06 12:35:03 +0100

    Update error handling

--- src/AutoTests.Framework.Components/ComponentsSteps.cs
+++ src/AutoTests.Framework.Components/ComponentsSteps.cs
@@ -4,6 +4,7 @@ using AutoTests.Framework.Components.Services;
 using AutoTests.Framework.Expressions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
@@ -39,6 +40,8 @@ public sealed class ComponentsSteps
     [Then(@"should have following values:")]
     public async Task ThenFieldsShouldHaveFollowingValues(Dictionary<ArgumentExpression, ArgumentExpression> values)
     {
+        var errors = new List<string>();
+
         foreach (var keyValuePair in values)
         {
             var path = await keyValuePair.Key.ExecuteAsync<string>();
@@ -47,34 +50,53 @@ public sealed class ComponentsSteps
 
             if ((expected == null && actual != null) || !expected!.Equals(actual))
             {
-                throw new Exception($"Unexpected value in component. Actual {actual}. Expected: {expected}");
+                errors.Add($"Path '{path}'. Actual '{actual}'. Expected: '{expected}'");
             }
+        }
+
+        if (errors.Any())
+        {
+            throw new Exception($"Some components have unexpected values: {string.Join(Environment.NewLine, errors)}");
         }
     }
 
     [Then(@"should be visible:")]
     public async Task ThenShouldBeVisible(IEnumerable<ArgumentExpression> argumentExpressions)
     {
+        var errors = new List<string>();
+
         foreach (var argumentExpression in argumentExpressions)
         {
             var path = await argumentExpression.ExecuteAsync<string>();
             if (!await componentService.GetComponent<IVisible>(path).IsVisibleAsync())
             {
-                throw new Exception($"{path} is invisible");
+                errors.Add(path);
             }
+        }
+
+        if (errors.Any())
+        {
+            throw new Exception($"Some components are invisible: {string.Join(",", errors)}");
         }
     }
 
     [Then(@"should be invisible:")]
     public async Task ThenShouldBeInvisible(IEnumerable<ArgumentExpression> argumentExpressions)
     {
+        var errors = new List<string>();
+
         foreach (var argumentExpression in argumentExpressions)
         {
             var path = await argumentExpression.ExecuteAsync<string>();
             if (await componentService.GetComponent<IVisible>(path).IsVisibleAsync())
             {
-                throw new Exception($"{path} is visible");
+                errors.Add(path);
             }
+        }
+
+        if (errors.Any())
+        {
+            throw new Exception($"Some components are visible: {string.Join(",", errors)}");
         }
     }
 }
