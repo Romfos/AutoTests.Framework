fe0d93a0f73bb69d1b70e6926484f0ba9e413f14
Romfos <Romfos@tut.by>
2023-12-23 02:14:50 +0100

    Make DataService Scenario level

--- src/AutoTests.Framework/Data/DataSpecflowHooks.cs
+++ src/AutoTests.Framework/Data/DataSpecflowHooks.cs
@@ -4,10 +4,10 @@ using TechTalk.SpecFlow;
 namespace AutoTests.Framework.Data;
 
 [Binding]
-public sealed class DataSpecflowHooks
+public sealed class DataSpecflowHooks(ITestRunnerManager testRunnerManager, IObjectContainer objectContainer)
 {
-    [BeforeTestRun]
-    public static void BeforeTestRun(ITestRunnerManager testRunnerManager, IObjectContainer objectContainer)
+    [BeforeScenario]
+    public void BeforeTestRun()
     {
         var dataLoader = objectContainer.Resolve<DataLoader>();
         var data = dataLoader.Load(testRunnerManager.BindingAssemblies);
